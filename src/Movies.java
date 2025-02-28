c8abfdec36a9fcb24037fdd5ac56b52bb5e2cd5d
Ani <a.alexandrova26@acsbg.org>
2025-02-28 08:47:55 +0200

    Initial commit

---
+++ src/Movies.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Movies {
+    private JPanel panel1;
+    private JPanel panel;
+    private JButton Title;
+    private JButton Genre;
+    private JTextField Title_result;
+    private JTextField Genre_result;
+    private JTable all_movies;
+}
